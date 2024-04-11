a55aff9a32c9979d01471418f0c04ecb2f7b9369
blvck29 <a20210850@pucp.edu.pe>
2024-04-10 20:59:50 -0500

    feat: controller + game

---
+++ Lab2/src/main/java/com/lab2/patitos/Controller/GameController.java
@@ -0,0 +1,27 @@
+package com.lab2.patitos.Controller;
+
+import com.lab2.patitos.Entity.GameConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "patito_hule", method = RequestMethod.GET)
+public class GameController {
+
+    @GetMapping(value = "")
+    public String main(){
+        return "game_config";
+    }
+
+    @PostMapping(value = "/configurando_juego")
+    public String recibirConfiguraciones(GameConfig configuracion){
+
+        configuracion.runGame();
+
+        return "game";
+    }
+
+}
---
+++ Lab2/src/main/java/com/lab2/patitos/Entity/GameConfig.java
@@ -0,0 +1,51 @@
+package com.lab2.patitos.Entity;
+
+import java.util.ArrayList;
+
+public class GameConfig {
+
+    private int numeroFilas;
+    private int numeroColumnas;
+    private String posicionesIniciales;
+    private int fotosTomadas;
+
+    public int getNumeroFilas() {
+        return numeroFilas;
+    }
+
+    public void setNumeroFilas(int numeroFilas) {
+        this.numeroFilas = numeroFilas;
+    }
+
+    public int getNumeroColumnas() {
+        return numeroColumnas;
+    }
+
+    public void setNumeroColumnas(int numeroColumnas) {
+        this.numeroColumnas = numeroColumnas;
+    }
+
+    public String getPosicionesIniciales() {
+        return posicionesIniciales;
+    }
+
+    public void setPosicionesIniciales(String posicionesIniciales) {
+        this.posicionesIniciales = posicionesIniciales;
+    }
+
+    public int getFotosTomadas() {
+        return fotosTomadas;
+    }
+
+    public void setFotosTomadas(int fotosTomadas) {
+        this.fotosTomadas = fotosTomadas;
+    }
+
+    public void runGame(){
+
+        
+
+    }
+
+
+}
